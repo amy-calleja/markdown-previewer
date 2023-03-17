4be584958e6cab4adcf3060503712e433db1f7a9
AmyCalleja <amy.calleja@live.com>
2023-03-17 17:46:26 +0100

    css adjustments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { marked } from 'marked';
+
 
 function App() {
-  const [edits, setEdits] = useState('')
+  const [edits, setEdits] = useState(`  # H1
+  ## H2
+  
+  A paragraph with *emphasis* and **strong importance**.
+
+  > A block quote with ~strikethrough~ and a link: [React.js](https://www.reactjs.org)
+  
+  * Lists of \`code\`
+  * [ ] todo
+  * [x] done
+  
+  > This is a blockquote
+
+    <html>
+      <head> Code blocks are fun! </head>
+    </html>
+  
+  ~~~
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  ~~~
+
+ ![rocket ship](https://img.fortawesome.com/1ce05b4b/ad-pro-visuals-cropped.svg)
+  
+
+`
+  )
 
   function handleChange(e) {
     setEdits(e.target.value)
   }
+
+const getMarkdownEdits=()=> {
+    <ReactMarkdown children={edits} remarkPlugins={[remarkGfm]} />
+  }
+
+  marked.setOptions({breaks: true});
+  
 
   return (
     <div className="App">
@@ -17,14 +57,14 @@ function App() {
         <div className='col-6'>
           <div className='editor-window'>
             <div className='toolbar text-start p-2'>Editor</div>
-              <textarea value={edits} onChange={handleChange} id="editor" >Text area Editor</textarea>
+              <textarea spellcheck="false" value={edits} onChange={handleChange} id="editor" text="editor">Text area Editor</textarea>
           </div>
         </div>
 
           <div className='col-6'>
             <div className='preview-window'>
                 <div className='toolbar text-start p-2'>Preview</div>
-                    <div id="preview">{edits}</div>
+                    <div id="preview" className='px-4 pb-5 text-start'><ReactMarkdown children={edits} remarkPlugins={[remarkGfm]}/></div>
                 </div>
             </div>
         </div>
